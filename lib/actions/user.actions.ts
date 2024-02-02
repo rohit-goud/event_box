86dd0caea92e588bcf1a4e1ee68c361702a6b1ef
rohit-goud <rohitnever@gmail.com>
2024-02-02 20:45:34 +0000

    final commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,11 +13,10 @@ import { CreateUserParams, UpdateUserParams } from '@/types'
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase()
+
     const newUser = await User.create(user)
-   
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
-   
     handleError(error)
   }
 }
