465131e7e08284457b347b574590b6b618ee806c
rohit-goud <rohitnever@gmail.com>
2024-02-03 13:47:40 +0000

    changes in profile page

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -16,7 +16,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const isEventCreator = userId === event.organizer._id.toString();
+  const isEventCreator = event && event.organizer && userId === event.organizer._id.toString();
 
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
@@ -27,7 +27,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
       />
       {/* IS EVENT CREATOR ... */}
 
-      {isEventCreator && !hidePrice && (
+      {isEventCreator && !hidePrice &&event&& (
         <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
           <Link href={`/events/${event._id}/update`}>
             <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
@@ -45,7 +45,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             {event.isFree ? 'FREE' : `$${event.price}`}
           </span>
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-            {event.category.name}
+          {event && event.category && event.category.name}
           </p>
         </div>}
 
@@ -59,7 +59,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
-            {event.organizer.firstName} {event.organizer.lastName}
+          {event && event.organizer && `${event.organizer.firstName} ${event.organizer.lastName}`} 
           </p>
 
           {hasOrderLink && (
